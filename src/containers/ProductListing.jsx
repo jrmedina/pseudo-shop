3aff07b55653982c3ff8c7e39592805b8ca4cdb6
jmedina <ndgns2@gmail.com>
2023-03-06 13:02:57 -0700

    Create axios call to  retrieve data

--- src/containers/ProductListing.jsx
+++ src/containers/ProductListing.jsx
@@ -1,14 +1,30 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import ProductComponent from "./ProductComponent";
-
+import { setProducts } from "../redux/actions/productActions";
+import axios from "axios";
 
 const ProductListing = () => {
-    const products = useSelector((state) => state)
+  const products = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const fetchProducts = async () => {
+    const response = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((err) => {
+        console.log("Error", err);
+      });
+    dispatch(setProducts(response.data));
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
 
   return (
     <div className="ui grid container">
-      <ProductComponent/>
+      <ProductComponent />
     </div>
   );
 };
