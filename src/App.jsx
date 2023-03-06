d9a52e9a07464e5ab3bb1f9756a96b990d7b38af
jmedina <ndgns2@gmail.com>
2023-03-06 12:25:45 -0700

    Add routes for component paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import "./App.css";
 import React from "react";
 import Header from "./containers/Header";
+import { Route, Switch } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import ProductDetails from "./containers/ProductDetails";
+
 const App = () => {
-  return <div>
-    <Header/>
-  </div>;
+  return (
+    <div className="App">
+    
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetails} />
+          <Route>404 Not Found!</Route>
+        </Switch>
+ 
+    </div>
+  );
 };
 
 export default App;
--- src/containers/ProductListing.jsx
+++ src/containers/ProductListing.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ProductListing = () => {
-  return <div>ProductListing</div>;
+  return (
+    <div>
+      <h1>ProductListing</h1>
+    </div>
+  );
 };
 
 export default ProductListing;
