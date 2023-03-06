3256e66648df5b727c856cf8c047ea54c05829de
jmedina <ndgns2@gmail.com>
2023-03-06 11:43:59 -0700

    Install devtools dependecies

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { productReducer } from "./productReducer";
 
-const reducers = combineReducers({ 
-    allProducts: productReducer 
+const reducers = combineReducers({
+  allProducts: productReducer,
 });
+
+export default reducers;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,10 @@
-import { configureStore } from "redux";
+import { createStore } from "redux";
 import reducers from "./reducers";
 
-const store = configureStore(reducers, {});
+const store = createStore(
+  reducers,
+  {},
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
